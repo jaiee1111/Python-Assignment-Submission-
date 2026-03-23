f9aef519ca92deac8ca3219107f2593d65ee8cbf
jaiee1111 <jaieededge@gmail.com>
2026-03-23 19:07:41 +0530

    Add files via upload

---
+++ unit_1/# tuple.py
@@ -0,0 +1,21 @@
+# tuple
+t1 = (10, 20, 30, 40)
+print("Tuple:", t1)
+
+print("Element at index 0:", t1[0])
+print("Element at index 2:", t1[2])
+
+# Nested tuple
+t2 = (("Amit", 101), ("Riya", 102))
+print("Nested Tuple:", t2)
+
+# Hashing
+print("Hash value of tuple:", hash(t1))
+
+# Concatenation
+t3 = t1 + (50, 60)
+print("After concatenation:", t3)
+
+# Repetition
+t4 = t1 * 2
+print("After repetition:", t4)
